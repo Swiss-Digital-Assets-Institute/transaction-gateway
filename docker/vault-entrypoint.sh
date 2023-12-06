9fd8cab5c20eb31ca44875ffe733b9d8ae955dfe
Volodymyr Sevastianov <voviksev@gmail.com>
2023-12-06 15:17:15 +0200

    remove comments

--- docker/vault-entrypoint.sh
+++ docker/vault-entrypoint.sh
@@ -9,13 +9,6 @@ export VAULT_ADDR='http://0.0.0.0:8200';
 
 vault operator init -key-threshold=1 -key-shares=1
 
-# set -m;
-# nohup vault server -dev -dev-root-token-id=${VAULT_DEV_ROOT_TOKEN_ID} -dev-listen-address=0.0.0.0:8200 > /dev/stdout &
-# sleep 5;
-
-# export VAULT_ADDR='http://0.0.0.0:8200';
-# export VAULT_TOKEN=${VAULT_DEV_ROOT_TOKEN_ID};
-
 vault status;
 
 fg %1;
