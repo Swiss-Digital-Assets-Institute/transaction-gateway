4d66c2096b2fb36619a48a5eab2a6f3d5401c835
Volodymyr Sevastianov <voviksev@gmail.com>
2023-12-15 14:52:31 +0200

    add correct data transformation and improve docs

--- packages/api-server/test/test-execute-transaction.ts
+++ packages/api-server/test/test-execute-transaction.ts
@@ -1,18 +1,20 @@
-import { AccountId, Client, PrivateKey, TopicCreateTransaction, TransactionId } from '@hashgraph/sdk';
+import { Client, PrivateKey, TopicCreateTransaction, TransactionId } from '@hashgraph/sdk';
 import axios from 'axios';
 
-const privateKey = PrivateKey.fromStringED25519('your PK');
+const privateKey = PrivateKey.fromStringED25519('your pk');
+const apiKey = 'de364303-0d0e-401f-838a-61d451d10d3a';
 
 const client = Client.forTestnet();
 
 async function request(): Promise<void> {
-  const operatorAccountId = await axios.get('http://localhost:3000/v1/transaction/operatorAccountId');
+  const operatorAccountId = await axios.get('http://localhost:3000/v1/transaction/operatorAccountId', {
+    headers: { Authorization: apiKey },
+  });
 
   const freezedTx = new TopicCreateTransaction()
     .setAdminKey(privateKey)
     .setSubmitKey(privateKey)
     .setTransactionId(TransactionId.generate(operatorAccountId.data.accountId))
-    .setNodeAccountIds([AccountId.fromString('0.0.3')])
     .freezeWith(client);
 
   const signedTx = await freezedTx.sign(privateKey);
@@ -22,7 +24,7 @@ async function request(): Promise<void> {
   const body = {
     transaction: txString,
   };
-  await axios.post('http://localhost:3000/v1/transaction/execute', body);
+  await axios.post('http://localhost:3000/v1/transaction/execute', body, { headers: { Authorization: apiKey } });
 }
 
 request().then(() => {
