795004fabc9e22443e61953638a43e700d98bfed
Volodymyr Sevastianov <voviksev@gmail.com>
2023-12-26 22:22:09 +0200

    fix launch commands

--- docker/create-app-roles.sh
+++ docker/create-app-roles.sh
@@ -5,31 +5,31 @@ vault auth enable approle;
 
 vault write auth/approle/role/executor \
     token_policies="executor" \
-    secret_id_ttl=8h \
-    token_num_uses=10 \
-    token_ttl=20m \
-    token_max_ttl=30m \
-    secret_id_num_uses=40;
+    secret_id_ttl=0 \
+    token_num_uses=0 \
+    token_ttl=0 \
+    token_max_ttl=0 \
+    secret_id_num_uses=0;
 vault read auth/approle/role/executor/role-id;
 vault write -f auth/approle/role/executor/secret-id;
 
 vault write auth/approle/role/refiller \
     token_policies="refiller" \
-    secret_id_ttl=8h \
-    token_num_uses=10 \
-    token_ttl=20m \
-    token_max_ttl=30m \
-    secret_id_num_uses=40;
+    secret_id_ttl=0 \
+    token_num_uses=0 \
+    token_ttl=0 \
+    token_max_ttl=0 \
+    secret_id_num_uses=0;
 vault read auth/approle/role/refiller/role-id;
 vault write -f auth/approle/role/refiller/secret-id;
 
 vault write auth/approle/role/cli \
     token_policies="cli" \
-    secret_id_ttl=8h \
-    token_num_uses=10 \
-    token_ttl=20m \
-    token_max_ttl=30m \
-    secret_id_num_uses=40;
+    secret_id_ttl=0 \
+    token_num_uses=0 \
+    token_ttl=0 \
+    token_max_ttl=0 \
+    secret_id_num_uses=0;
 vault read auth/approle/role/cli/role-id;
 vault write -f auth/approle/role/cli/secret-id;
 vault secrets enable -path=secret kv-v2;
